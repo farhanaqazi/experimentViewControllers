75d646f6ba8a2fa25aded8ce8bf75f653ee36752
FarhanaQazi <farhan.abu.qazi@att.net>
2018-07-03 20:55:47 -0400

    This is the First Try with all 3 view controllers in one view. This version needs some constraints work.

--- experimentViewControllers/ViewController.swift
+++ experimentViewControllers/ViewController.swift
@@ -13,13 +13,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("View Did load, did load")
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Mark: Image Picker View Controller
+    @IBAction func experiment(_ sender: UIButton) {
+        
+        let nextController = UIImagePickerController()
+        
+        present(nextController, animated: true, completion: nil)
+        
+    print("Image Picker Button pressed")
+    
     }
+     // Mark: Activity View Controller
+    @IBAction func experiment2()  {
+        
+        let image = UIImage()
+        let actController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        present(actController, animated: true, completion: nil)
+        
+        print("Act View Controller Button pressed")
+        
+    }
+    // Mark: Alert View Controller
+    
+    @IBAction func experiment3() {
+    
+        let alertController = UIAlertController()
+        
+        alertController.title = " Just Testing this Alert"
+        alertController.message = "This just a test"
+        
+        let okAction = UIAlertAction(title:"ok", style: UIAlertActionStyle.default)
+        { action in self.dismiss(animated: true, completion: nil)
+        }
+       
+        
+        alertController.addAction(okAction)
+   
+         present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    
 
 
 }
-
